c9fa2ac5bfd2e04e19f5f9d1870b8596cf2cd5a4
Daniel Miller <danmiller@hey.com>
2021-06-24 14:59:00 -0700

    add type definition for global prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,13 @@
 import { PrismaClient } from '@prisma/client'
 
 let prisma: PrismaClient
+declare global {
+  namespace NodeJS {
+    interface Global {
+      prisma: any;
+    }
+  }
+}
 
 
 if (process.env.NODE_ENV === 'production') {
