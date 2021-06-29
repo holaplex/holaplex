f5f40c374a44bc6255eeb4652fd931fafa706ffd
Damian Rebman <damian.rebman@gmail.com>
2021-06-28 20:37:26 -0700

    Removes name step, fixes font pickers

--- pages/builderForm/CustomizeStore.tsx
+++ pages/builderForm/CustomizeStore.tsx
@@ -1,4 +1,4 @@
-// @ts-nocheck 
+// @ts-nocheck
 import {useState} from 'react';
 import sv from '../../constants/Styles'
 import styled from 'styled-components';
@@ -120,6 +120,13 @@ const CustomizeStore = ({nextAction, backAction}: Props) => {
             font={titleFont}
           />
         </Field>
+        <Field>
+          <FontPicker
+            label="Main Text Font"
+            pickerId="body"
+            font={textFont}
+          />
+        </Field>
         <Actions>
           <Button
             label="Back"
@@ -135,7 +142,6 @@ const CustomizeStore = ({nextAction, backAction}: Props) => {
       </Form>
       {/* @ts-ignore */}
       <Preview bgColor={backgroundColor}>
-        <div className="apply-font">Does this work???</div>
         <PreviewItem>
           <H2 className="apply-font-title" color={textColor}>Big Title</H2>
         </PreviewItem>
@@ -155,7 +161,7 @@ const CustomizeStore = ({nextAction, backAction}: Props) => {
         </PreviewItem>
         <PreviewItem>
           <PreviewButton
-            className="body-font"
+            className="apply-font-body"
             color={primaryColor}
             textColor={buttontextColor}
           >
