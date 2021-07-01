f37ccfda38be1a0cc59fee5ea6c159c1c0219cc8
Daniel Miller <danmiller@hey.com>
2021-07-01 14:04:27 -0700

    remove shading function

--- src/modules/theme/index.ts
+++ src/modules/theme/index.ts
@@ -57,30 +57,4 @@ h4,
   color: ${primaryColor};
 }
 ` 
-<<<<<<< HEAD
   }
-=======
-  }
-
-  function shadeColor(color: string, percent: number) {
-    // https://stackoverflow.com/questions/5560248/programmatically-lighten-or-darken-a-hex-color-or-rgb-and-blend-colors
-  
-    let R = parseInt(color.substring(1,3),16);
-    let G = parseInt(color.substring(3,5),16);
-    let B = parseInt(color.substring(5,7),16);
-  
-    R = R * (100 + percent) / 100;
-    G = G * (100 + percent) / 100;
-    B = B * (100 + percent) / 100;
-  
-    R = (R<255)?R:255;  
-    G = (G<255)?G:255;  
-    B = (B<255)?B:255;  
-  
-    const RR = ((R.toString(16).length==1)?"0"+R.toString(16):R.toString(16));
-    const GG = ((G.toString(16).length==1)?"0"+G.toString(16):G.toString(16));
-    const BB = ((B.toString(16).length==1)?"0"+B.toString(16):B.toString(16));
-  
-    return "#"+RR+GG+BB;
-  }
->>>>>>> 8d078afe40bee1f71970dda98ac81bfe0d2d899e
