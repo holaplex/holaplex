5f0e4113ffbd0e896de711105f7317f9d95266cf
Kyle Espinola <kyle.s.espinola@gmail.com>
2021-07-16 13:28:35 -0700

    fix: use next js version of ngnix config

--- server.ts
+++ server.ts
@@ -4,11 +4,15 @@ import { parse } from 'url'
 import fs from 'fs'
 import next from 'next'
 
-const port = 3000
+let port = parseInt(process.env.PORT || '3000', 10)
 const production = process.env.NODE_ENV === 'production'
 const dev = !production
 const app = next({ dev })
 const handle = app.getRequestHandler()
+
+if (production) {
+  port = 3000
+}
 
 app.prepare().then(() => {
   createServer((req, res) => {
