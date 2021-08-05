e1d96c3405538b3fb853041f276cb367142aeb2a
Kyle Espinola <kyle.s.espinola@gmail.com>
2021-08-05 11:36:42 -0700

    fix: ant theme overrides setting dark theme defaults before overriding

--- src/modules/theme/index.ts
+++ src/modules/theme/index.ts
@@ -28,7 +28,11 @@ export function variables({ backgroundColor, primaryColor, textFont, titleFont, 
     buttonTextColor = '#FFFFFF'
   }
 
-  const themeVariables = `@body-background: ${backgroundColor};
+  const themeVariables = `@import '~antd/es/style/themes/dark.less';
+@import '~antd/dist/antd.dark.less';
+
+@import url('https://fonts.googleapis.com/css2?family=${joinFonts([titleFont, textFont])}&display=swap');
+@body-background: ${backgroundColor};
 @text-color: ${textColor};
 @component-background: ${backgroundColor};
 `
