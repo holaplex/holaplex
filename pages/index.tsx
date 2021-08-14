43b276187862107781dd8560dfc481665a31d6bb
damianr <damian@Damians-iMac.attlocal.net>
2021-08-13 20:31:22 -0700

    wip: images are broken. help me Dan

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,19 @@
 import React, { useContext } from 'react'
 import Image from 'next/image'
 import styled from 'styled-components'
+import sv from '@/constants/styles'
 import WavesSection from '@/assets/images/wave-section.svg'
-import { Space, Row, Col } from 'antd'
+import Tester from '../public/demo-logo.png'
+import HighlightStores from '@/assets/highlight-stores/highlight-stores-stub'
+import HandLogo from '@/assets/images/hola-logo.svg'
+import { Space, Row, Col, Typography, Card } from 'antd'
 import Button from '@/components/elements/Button'
 import { WalletContext } from '@/modules/wallet'
 
+const {Title, Text} = Typography;
+
 const Logo = styled.div`
   margin: 70px 0 0 0;
-  font-size: 90px;
-  line-height: 90px;
 `;
 
 const HeroTitle = styled.h1`
@@ -18,7 +22,15 @@ const HeroTitle = styled.h1`
   font-size: 68px;
   line-height: 68px;
   color: #fff;
-`
+`;
+
+const LightText = styled(Text)`
+  color: rgba(255,255,255,.6);
+`;
+
+const LightTitle = styled(Title)`
+  color: #ffffff !important;
+`;
 
 const Pitch = styled.h2`
   font-size: 32px;
@@ -31,20 +43,80 @@ const Pitch = styled.h2`
 
 const VideoSection = styled(Row)`
   background-image: url('${WavesSection}');
+  margin-top: ${sv.sectionPadding}px;
+  padding: ${sv.sectionPadding*2.5}px 0;
+`;
+
+const Video = styled.div`
+  border-radius: ${sv.grid}px;
+  overflow: hidden;
   position: relative;
-  height: 800px;
+  padding: 0 0 56.25% 0;
+  min-height: 100px;
+  box-shadow: 0 0 8px rgba(0,0,0,.2);
+  .video {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border: 0;
+  }
+`;
+
+const VideoPitch = styled.div`
+  height: 100%;
+  ${sv.flexColumn};
+  justify-content: center;
+`;
+
+const Community = styled(Row)`
+  margin-top: ${sv.sectionPadding}px;
+`;
+
+const ComTitle = styled(LightTitle)`
+  text-align: center;
+`;
+
+const Stores = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-column-gap: ${sv.grid*2}px;
+  width: 100%;
+`;
+
+const StoreItem = styled(Card)`
+  width: 100%;
+  overflow: hidden;
+  margin-bottom: ${sv.grid*2}px;
+  .ant-card-body {
+    padding: 0;
+  }
+`;
+
+const StoreImage = styled.div`
+  width: 100%;
+  height: 212px;
+  background: aqua;
+  background-image: url('${props => props.image}');
+`;
+
+const StoreName = styled.div`
+  color: ${sv.colors.text};
 `;
 
 
-
 export default function Home() {
+  console.log(WavesSection)
   const { solana, arweaveWallet, connect } = useContext(WalletContext)
   return (
     <>
       <Row justify="center">
         <Col sm={16} md={14} lg={12} xl={10}>
           <Space direction="vertical" align="center" size="large">
-            <Logo>👋</Logo>
+            <Logo>
+              <Image width={90} height={90} src={HandLogo} />
+            </Logo>
             <HeroTitle>Holaplex</HeroTitle>
             <Pitch>Design, launch, and host your Metaplex NFT marketplace. No coding required!</Pitch>
             {solana && arweaveWallet && (
@@ -55,10 +127,45 @@ export default function Home() {
           </Space>
         </Col>
       </Row>
-      <VideoSection>
-        ballz
-        {/* <BackgroundWaves src={WavesSection} /> */}
+
+      <VideoSection justify="center">
+        <Col sm={20} md={18} lg={16} xl={16}>
+          <Row gutter={24}>
+            <Col span={12}>
+              <VideoPitch direction="vertical">
+                <LightTitle level={2}>Build your store in about 5 minutes</LightTitle>
+                <LightText>Let’s be honest, nobody wants to spend weeks building a store. That’s why our team got together to create Holaplex - the no-code NFT store builder. Create a store in a couple of clicks so you can start selling!</LightText>
+              </VideoPitch>
+            </Col>
+            <Col span={12}>
+              <Video>
+                <iframe className="video" src="https://www.youtube.com/embed/3AEqDfN-lX8" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+              </Video>
+            </Col>
+          </Row>
+        </Col>
       </VideoSection>
+
+      <Community justify="center">
+        <Col sm={20} md={18} lg={16} xl={16}>
+          <ComTitle level={2}>Join our community, 129 stores and counting!</ComTitle>
+
+          <Stores>
+            {HighlightStores.map((store, index) => {
+              console.log(store)
+              return (
+                <StoreItem>
+                  {/* <img src="../public/demo-logo.png" /> */}
+                  <img src={store.image} />
+                  <StoreImage image={store.image} />
+                  <StoreName>{store.name}</StoreName>
+                </StoreItem>
+              )
+            })}
+          </Stores>
+
+        </Col>
+      </Community>
     </>
   )
 }
---
+++ src/common/assets/highlight-stores/highlight-stores-stub.ts
@@ -0,0 +1,27 @@
+import solgangstaImage from '@/assets/highlight-stores/images/solgangsta.jpg'
+import cosmicBloomImage from '@/assets/highlight-stores/images/cosmic-bloom.jpg'
+import cryptoMonksImage from '@/assets/highlight-stores/images/crypto-monks.jpg'
+import lobsterShackImage from '@/assets/highlight-stores/images/lobster-shack.jpg'
+import paausmusicImage from '@/assets/highlight-stores/images/paausmusic.jpg'
+import splCardsImage from '@/assets/highlight-stores/images/spl-cards.jpg'
+import topHatChickImage from '@/assets/highlight-stores/images/top-hat-chicks.jpg'
+
+const stores = [
+  {
+     name: 'Solgangsta',
+     url: 'https://solgangsta.holaplex.com/',
+     image: solgangstaImage
+  },
+  {
+     name: 'Top Hat Chicks',
+     url: 'https://tophatchicks.holaplex.com/#/',
+     image: topHatChickImage
+  },
+  {
+     name: 'Crypto Monks',
+     url: 'https://cryptomonks.holaplex.com/#/',
+     image: cryptoMonksImage
+  },
+]
+
+export default stores
