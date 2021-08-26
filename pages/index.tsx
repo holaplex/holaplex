7b57cc346ed89fb9eda1d06820b34c4341ab02a9
Kyle Espinola <kyle.s.espinola@gmail.com>
2021-08-26 09:24:24 -0700

    fix: final touches to index. Have site work in incognito browsers.

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,15 +5,14 @@ import sv from '@/constants/styles'
 import SocialLinks from '@/components/elements/SocialLinks'
 import WavesSection from '@/assets/images/wave-section.svg'
 import WavesFooter from '@/assets/images/wave-footer.svg'
-import HighlightStores from '@/assets/highlight-stores/highlight-stores-stub'
+import higlightStores from '@/assets/highlight-stores/highlight-stores-stub'
 import HandLogo from '@/assets/images/hola-logo.svg'
-import { Space, Row, Col, Typography, Card } from 'antd'
+import { List, Space, Row, Col, Typography, Card } from 'antd'
 import Link from 'next/link'
 import Button from '@/components/elements/Button'
 import { WalletContext } from '@/modules/wallet'
-import useWindowDimensions from '@/hooks/useWindowDimensions'
 
-const { Title, Text } = Typography;
+const { Title, Text, Paragraph } = Typography;
 
 const Logo = styled.div`
   margin: 70px 0 0 0;
@@ -27,7 +26,7 @@ const HeroTitle = styled.h1`
   color: #fff;
 `;
 
-const LightText = styled(Text)`
+const LightText = styled(Paragraph)`
   color: rgba(255,255,255,.6);
   a {
     color: rgba(255,255,255,1);
@@ -40,7 +39,9 @@ const LightText = styled(Text)`
 `;
 
 const LightTitle = styled(Title)`
-  color: #ffffff !important;
+  &.ant-typography, &.ant-typography {
+    color: #ffffff;
+  }
 `;
 
 const WhiteButton = styled(Button)`
@@ -125,6 +126,9 @@ type StoreImageProps = {
   image: string;
 }
 
+const FinalCall = styled(Row)`
+  margin: 0 0 100px 0;
+`
 const StoreImage = styled.div<StoreImageProps>`
   width: 100%;
   height: 212px;
@@ -141,22 +145,8 @@ const StoreName = styled.div`
 
 const Footer = styled(Row)`
   background-image: url('${WavesFooter}');
-  margin-top: ${sv.sectionPadding}px;
-  padding-top: ${sv.sectionPadding * 2.5}px;
-`;
-
-const CenteredTitle = styled(LightTitle)`
-  text-align: center;
-`;
-
-const FooterLinks = styled(Col)`
-  height: 80px;
-  ${sv.flexRow};
-  margin-top: ${sv.sectionPadding}px;
-`;
-
-const FooterSection = styled.div`
-  flex: 1;
+  margin: 112px 0 0 0;
+  padding: 280px 0 60px 0;
   color: rgba(253, 253, 253, 0.6);
   a {
     color: rgba(253, 253, 253, 0.6);
@@ -166,15 +156,14 @@ const FooterSection = styled.div`
   }
 `;
 
+const CenteredTitle = styled(LightTitle)`
+  text-align: center;
+`;
+
+
 export default function Home() {
-  const { solana, arweaveWallet, connect } = useContext(WalletContext)
-  const windowDimensions = useWindowDimensions();
-  let storesPerRow = 4
-  if (windowDimensions.width < 400) {
-    storesPerRow = 1
-  } else if (windowDimensions.width < 880) {
-    storesPerRow = 2
-  }
+  const { connect } = useContext(WalletContext)
+
   return (
     <>
       <Row justify="center">
@@ -195,22 +184,17 @@ export default function Home() {
       <VideoSection justify="center">
         <Col sm={20} md={18} lg={16} xl={16}>
           <Row gutter={24}>
-            <Col span={windowDimensions.width > 860 ? 12 : 24}>
+            <Col xs={24} md={10} lg={10} xl={10} xxl={10}>
               <VideoPitch>
                 <LightTitle level={2}>Build your store in about 5 minutes</LightTitle>
                 <LightText>Let’s be honest, nobody wants to spend weeks building a store. That’s why our team got together to create Holaplex - the no-code NFT store builder. Create a store in a couple of clicks so you can start selling! Need some guidance?  Try our <a href="https://docs.google.com/document/d/1fggieMGqgJqfp-TDoSsoeqS38iKPKcb1tTfxBkPXbeM/edit#" target="_blank" rel="noreferrer">getting started guide.</a></LightText>
               </VideoPitch>
-              {windowDimensions.width < 860 &&
-                <WhiteButton size="large" onClick={() => connect()}>Watch a Video</WhiteButton>
-              }
             </Col>
-            {windowDimensions.width > 860 &&
-              <Col span={12}>
-                <Video>
-                  <iframe className="video" src="https://www.youtube.com/embed/3AEqDfN-lX8" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-                </Video>
-              </Col>
-            }
+            <Col xs={24} md={12} lg={12} xl={12} xxl={12} offset={2}>
+              <Video>
+                <iframe className="video" src="https://www.youtube.com/embed/3AEqDfN-lX8" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+              </Video>
+            </Col>
           </Row>
         </Col>
       </VideoSection>
@@ -218,22 +202,24 @@ export default function Home() {
       <Community justify="center">
         <Col sm={20} md={20} lg={20} xl={18}>
           <CenteredTitle level={2}>Join our community, 150 stores and counting!</CenteredTitle>
-
-          <Stores storesPerRow={storesPerRow}>
-            {HighlightStores.map((store, index) => (
-              <a
-                key={index}
-                href={store.url}
-                target="_blank"
-                rel="noreferrer"
-              >
-                <StoreItem>
-                  <StoreImage image={store.imagePath} />
-                  <StoreName>{store.name}</StoreName>
-                </StoreItem>
-              </a>
-            ))}
-          </Stores>
+          <List
+            grid={{ xs: 1, sm: 2, md: 4, lg: 4, xl: 4, xxl: 4, gutter: 16 }}
+            dataSource={higlightStores}
+            renderItem={(store: { url: string, imagePath: string, name: string }) => (
+              <List.Item key={store.url}>
+                <a
+                  href={store.url}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <StoreItem>
+                    <StoreImage image={store.imagePath} />
+                    <StoreName>{store.name}</StoreName>
+                  </StoreItem>
+                </a>
+              </List.Item>
+            )}
+          />
           <Row justify="center">
             <LightText>
               <Link href="/storefronts">View More Stores</Link>
@@ -244,23 +230,32 @@ export default function Home() {
       </Community>
 
       <Footer justify="center">
-        <Space direction="vertical" align="center">
-          <CenteredTitle level={3}>Launch your own NFT store today!</CenteredTitle>
-          <Space direction="horizontal" size="large">
-            <WhiteButton size="large" onClick={() => connect()}>Create Your Store</WhiteButton>
-          </Space>
-        </Space>
-        <FooterLinks span={22}>
-          <FooterSection style={{ textAlign: 'left' }}>
-            <a href="mailto:hola@holaplex.com">hola@holaplex.com</a>
-          </FooterSection>
-          {windowDimensions.width > 800 &&
-            <FooterSection style={{ textAlign: 'center' }}>Made with &#10084; on <a href="https://www.metaplex.com" target="_blank" rel="noreferrer">Metaplex</a></FooterSection>
-          }
-          <FooterSection style={{ textAlign: 'right' }}>
-            <SocialLinks />
-          </FooterSection>
-        </FooterLinks>
+        <Col xs={18}>
+          <FinalCall justify="center">
+            <Space direction="vertical" align="center">
+              <CenteredTitle level={3}>Launch your own NFT store today!</CenteredTitle>
+              <WhiteButton size="large" onClick={() => connect()}>Create Your Store</WhiteButton>
+            </Space>
+          </FinalCall>
+          <Row>
+            <Col md={8}>
+              <a href="mailto:hola@holaplex.com">hola@holaplex.com</a>
+
+            </Col>
+            <Col md={8}
+            >
+              <Row justify="center">
+                Made with &#10084; on &#160;<a href="https://www.metaplex.com" target="_blank" rel="noreferrer">Metaplex</a>
+              </Row>
+            </Col>
+            <Col md={8}
+            >
+              <Row justify="end">
+                <SocialLinks />
+              </Row>
+            </Col>
+          </Row>
+        </Col>
       </Footer>
     </>
   )
