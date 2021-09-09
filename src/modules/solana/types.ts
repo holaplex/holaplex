540d7f05f8cea2b329ac4f7df1c17a1b81a8419a
ryans <ryans@opuslogica.com>
2021-09-09 11:34:02 -0700

    Add endpoint for uploading store without ArConnect

--- src/modules/solana/types.ts
+++ src/modules/solana/types.ts
@@ -1,9 +1,16 @@
+import { PublicKey, Transaction } from '@solana/web3.js';
+
 export type SolanaConnectOptions = {
   onlyIfTrusted?: boolean;
-}
+};
 
 export interface SolanaPublicKey {
-  toString: () => string,
+  toString: () => string;
+}
+
+export interface Signature {
+  publicKey: PublicKey;
+  signature: Buffer;
 }
 
 export interface Solana {
@@ -13,4 +20,6 @@ export interface Solana {
   once: (event: string, cb: () => void) => void;
   publicKey: SolanaPublicKey;
   connect: (options?: SolanaConnectOptions) => Promise<any>;
+  signTransaction: (tx: Transaction) => Promise<Transaction>;
+  signMessage: (input: ArrayBuffer, enc: string) => Promise<Signature>;
 }
