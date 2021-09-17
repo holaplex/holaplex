2bbd39f937e2f351b115d82157ea834667b6eee8
Daniel Miller <danieladammiller@gmail.com>
2021-09-17 14:05:28 -0700

    add basic script runner for store updating

--- next.config.js
+++ next.config.js
@@ -4,4 +4,12 @@ const withImages = require('next-images')
 module.exports = withImages(withAntdLess({
   reactStrictMode: true,
   lessVarsFilePath: './ant-theme.less',
+
+  webpack: function (config) {
+    return Object.assign({}, config, { entry: function() {
+      return config.entry().then((entry) => {
+        return Object.assign({}, entry, { updateStores: './src/scripts/update-stores' })
+      })
+    }})
+  }
 }))
