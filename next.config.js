41d018bbe0628fd6cbd62da94348a4b8fa866c82
Kyle Espinola <kyle.s.espinola@gmail.com>
2021-10-05 13:44:05 -0700

    refactor: retrieve arweave client from next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
 const withAntdLess = require('next-plugin-antd-less');
 const withImages = require('next-images')
+const withArweave = require('./src/modules/next/plugins/arweave')
+const { SecretsManagerClient } = require('@aws-sdk/client-secrets-manager');
 
-module.exports = withImages(withAntdLess({
-  reactStrictMode: true,
-  lessVarsFilePath: './ant-theme.less',
-}))
+const client = new SecretsManagerClient({
+  region: process.env.AWS_REGION,
+});
+
+module.exports = withImages(
+  withAntdLess(
+    withArweave(client, process.env.ARWEAVE_SECRET_ID)(
+      {
+        reactStrictMode: true,
+        lessVarsFilePath: './ant-theme.less',
+      }
+    )
+  )
+)
