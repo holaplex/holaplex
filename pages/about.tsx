c234dfdbfe5de3aef2d674e3ab4f917a64b411f7
damianr <damian.rebman@gmail.com>
2021-11-08 21:13:01 -0800

    fix(layout): fixes layout issues on about and index pages

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,24 +1,38 @@
 import React, { useContext } from 'react'
+import Image from 'next/image'
 import styled from 'styled-components'
 import sv from '@/constants/styles'
 import RoadmapImage from '@/assets/images/roadmap-v1.svg'
 import investorData from '@/assets/investors/investors-stub';
-import { List, Space, Row, Col, Typography, Image, ListProps } from 'antd'
-
+import { List, Space, Row, Col, Typography, Card } from 'antd'
+import Link from 'next/link'
+import Button from '@/components/elements/Button'
 const { Title, Text, Paragraph } = Typography;
 
-const Roadmap = styled(Image)`
+
+const ContentCol = styled(Col)`
+  max-width: 1400px;
+`;
+
+const Roadmap = styled.div`
   margin: 70px 0 0 0;
+  width: 100%;
+  overflow-x: auto;
+  padding: 0 ${sv.appPadding}px;
 `;
 
 const HeroTitle = styled.h1`
   text-align: center;
   font-weight: 800;
+  font-size: 68px;
   line-height: auto;
-  color: #fff;
-  font-size: 48px;
-  @media screen and (min-width: 800px) {
-    font-size: 68px;
+  margin-top: ${sv.sectionPadding}px;
+  @media screen and (max-width: 550px) {
+    margin-top: ${sv.appPadding}px;
+    font-size: 48px;
+    line-height: auto;
+    text-align: left;
+    padding: 0 ${sv.appPadding}px;
   }
 `;
 
@@ -36,6 +50,7 @@ const Pitch = styled.h2`
   }
 `
 
+
 const LightText = styled(Paragraph)`
   color: rgba(255,255,255,.6);
   a {
@@ -48,9 +63,21 @@ const LightText = styled(Paragraph)`
   }
 `;
 
-const Community = styled(Row)`
-  margin: 124px 0 84px 0;
+const LightTitle = styled(Title)`
+  &.ant-typography, &.ant-typography {
+    color: #ffffff;
+  }
 `;
+
+const Community = styled(Row)`
+  margin-top: ${sv.sectionPadding*2}px;
+  @media screen and (max-width: 550px) {
+    margin-top: ${sv.sectionPadding}px;
+    padding: 0 ${sv.appPadding}px;
+  }
+`;
+
+const Stats = styled.div``;
 
 const Stat = styled(Col)`
   text-align: center;
@@ -74,82 +101,116 @@ const StatTitle = styled.h1`
   }
 `;
 
-const Investors = styled.div`
-  background: #fff;
-  border-radius: 8px;
-  padding: 24px;
-  .ant-col {
-    > .ant-list-item {
-      text-align: center;
-      display: flex;
-      align-items: center;
-      flex-direction: column;
-      margin: 0 0 22px 0;
-    }
-  }
+const BackedBy = styled(Row)`
+  margin: ${sv.sectionPadding*2}px 0;
 `;
 
+const Investors = styled.div`
+  width: 100%;
+  background: #fff;
+  border-radius: 8px;
+  margin-top: ${sv.appPadding}px;
+`;
+
+const Investor = styled.div`
+  margin: ${sv.grid*4}px;
+  position: relative;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const LogoContainer = styled.a`
+  width: 100%;
+  height: 100%;
+  max-width: 120px;
+  display: block;
+  position: relative;
+`;
 
 export default function About() {
+
   return (
-    <Row justify="center" align="middle">
-      <Col xs={22} sm={22} md={20} xl={18} xxl={16}>
-        <Space direction="vertical" align="center" size="large">
-          <HeroTitle>Empowering a community of thousands of creators. </HeroTitle>
-          <Pitch>We’re building a suite of no-code required tools to enable creators and collectors to mint, discover, and sell NFTs.</Pitch>
-        </Space>
+    <>
+      <Row justify="center">
+        <ContentCol xs={22} md={20}>
+          <Space direction="vertical" align="center" size="large">
+            <HeroTitle>Building the best NFT ecosystem on the blockchain.</HeroTitle>
+            <Pitch>Our mission is to empower creators and collectors by building a suite of integrated tools to mint, discover, and sell NFTs.</Pitch>
+          </Space>
+        </ContentCol>
+      </Row>
 
-        <Roadmap preview={false} width="100%" src={RoadmapImage} alt="holaplex roadmap" />
+      <Row justify="center">
+        <Col>
+          <Roadmap>
+            <Image layout="fixed" width={1147} height={185} src={RoadmapImage} alt="hola" />
+          </Roadmap>
+        </Col>
+      </Row>
 
-        <Community>
-          <Col xs={24} md={12} lg={12} xl={12} xxl={12}>
-            <Title level={2}>We exist for our community of creators.</Title>
-            <LightText>The rapid rise of NFTs has allowed creators to build communities and monetize their work in  innovative ways. So much talent has come into this space but there are barriers preventing some from joining. Using the tools needed to mint and sell NFTs is non-trivial even for experienced developers.</LightText>
-            <LightText>Holaplex is committed to building tools that will allow creators and collectors to join the NFT community easily and safely.</LightText>
-          </Col>
-          <Col xs={24} md={12} lg={12} xl={12} xxl={12}>
-            <Row>
-              <Stat span={12}>
-                <StatTitle textColor={sv.colors.mainGradient}>1.5k+</StatTitle>
-                <LightText>Stores created</LightText>
-              </Stat>
-              <Stat span={12}>
-                <StatTitle textColor={sv.colors.orangeGradient}>5k+</StatTitle>
-                <LightText>NFTs minted</LightText>
-              </Stat>
-            </Row>
-            <Row>
-              <Stat span={12}>
-                <StatTitle textColor={sv.colors.orangeGradient}>8k+</StatTitle>
-                <LightText>Twitter followers</LightText>
-              </Stat>
-              <Stat span={12}>
-                <StatTitle textColor={sv.colors.greenGradient}>40k+</StatTitle>
-                <LightText>SOL transaction volume</LightText>
-              </Stat>
-            </Row>
-          </Col>
-        </Community>
+      <Community justify="center">
+        <ContentCol xs={22} md={20}>
+          <Row>
+            <Col xs={24} md={12} lg={12} xl={12} xxl={12}>
+              <LightTitle level={2}>We exist for our community of creators.</LightTitle>
+              <LightText>The rapid rise of NFTs has allowed creators to build communities and monetize their work in  innovative ways. So much talent has come into this space but there are barriers preventing some from joining. Using the tools needed to mint and sell NFTs is non-trivial even for experienced developers.</LightText>
+              <LightText>Holaplex is committed to building tools that will allow creators and collectors to join the NFT community easily and safely.</LightText>
+            </Col>
+            <Col xs={24} md={12} lg={12} xl={12} xxl={12}>
+              <Stats>
+                <Row>
+                  <Stat span={12}>
+                    <StatTitle textColor={sv.colors.mainGradient}>1.5k+</StatTitle>
+                    <LightText>Stores created</LightText>
+                  </Stat>
+                  <Stat span={12}>
+                    <StatTitle textColor={sv.colors.orangeGradient}>5k+</StatTitle>
+                    <LightText>NFTs minted</LightText>
+                  </Stat>
+                </Row>
+                <Row>
+                  <Stat span={12}>
+                    <StatTitle textColor={sv.colors.purpleGradient}>8k+</StatTitle>
+                    <LightText>Twitter followers</LightText>
+                  </Stat>
+                  <Stat span={12}>
+                    <StatTitle textColor={sv.colors.greenGradient}>40k+</StatTitle>
+                    <LightText>SOL transaction volume</LightText>
+                  </Stat>
+                </Row>
+              </Stats>
+            </Col>
+          </Row>
+        </ContentCol>
+      </Community>
 
-        <Title level={2} >Backed by</Title>
-        <Investors>
-          <List
-            grid={{ xs: 1, sm: 2, md: 3, lg: 4, xl: 4, xxl: 4, gutter: 28 }}
-            dataSource={investorData}
-            renderItem={(investor) => (
-              <List.Item key={investor.url}>
-                <a
-                  href={investor.url}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <Image preview={false} src={investor.logo} alt={investor.name} />
-                </a>
-              </List.Item>
-            )}
-          />
-        </Investors>
-      </Col>
-    </Row>
+      <BackedBy justify="center">
+        <ContentCol xs={22} md={20}>
+          <Title level={2}>Backed by</Title>
+          <Investors>
+            <List
+              grid={{ xs: 2, sm: 3, md: 4, lg: 4, xl: 4, xxl: 4, gutter: 16 }}
+              dataSource={investorData}
+              renderItem={(investor: { url: string, logo: string, name: string }) => (
+                <List.Item key={investor.url}>
+                  <Investor>
+                    <LogoContainer
+                      href={investor.url}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <Image layout="fill" objectFit="contain" src={investor.logo} alt={investor.name} />
+                    </LogoContainer>
+                  </Investor>
+                </List.Item>
+              )}
+            />
+          </Investors>
+        </ContentCol>
+      </BackedBy>
+
+    </>
   )
 }
