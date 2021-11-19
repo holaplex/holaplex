b24dd72860f11d285e40a54a7617ffd3c08f30dc
David Torbeck <davetorbeck@gmail.com>
2021-11-19 18:02:10 -0500

    Set max supply default to 0

--- src/modules/nfts/components/wizard/RoyaltiesCreators.tsx
+++ src/modules/nfts/components/wizard/RoyaltiesCreators.tsx
@@ -377,7 +377,7 @@ export default function RoyaltiesCreators({
   const [totalRoyaltyShares, setTotalRoyaltiesShare] = useState<number>(0);
   const [showErrors, setShowErrors] = useState<boolean>(false);
   const [editionsSelection, setEditionsSelection] = useState('one');
-  const [maxSupply, setMaxSupply] = useState<number>(1);
+  const [maxSupply, setMaxSupply] = useState<number>(0);
 
   useEffect(() => {
     // When creators changes, sum up all the shares.
