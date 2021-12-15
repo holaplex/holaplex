1ce79644a57f763f52b65900cd7f05d3f7559d0e
sjc <email@sjc.sh>
2021-12-15 15:31:00 -0600

    heroku css test 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -78,8 +78,10 @@ const FeaturedStores = styled(List)<ListProps<StorefrontFeature>>`
   }
 `;
 
-const CenteredContentCol = styled(ContentCol)`
+const CenteredContentCol = styled.div`
   margin: 0 auto;
+  max-width: 100vw;
+  padding: 0 1.5rem;
 `;
 
 export async function getStaticProps() {
