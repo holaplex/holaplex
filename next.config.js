ecd8042ae61bec1a4f5485cf4eb7db9385a97af5
David Torbeck <davetorbeck@gmail.com>
2022-01-19 17:15:41 -0600

    cleanup next config

--- next.config.js
+++ next.config.js
@@ -1,18 +1,6 @@
 const withImages = require('next-images');
-const withAntdLess = require('next-plugin-antd-less');
-
 const withPlugins = require('next-compose-plugins');
 const withLess = require('next-with-less');
-
-// Add your plugins here
-// const plugins = [
-//   withAntdLess,
- 
-// ];
-const lessToJS = require('less-vars-to-js');
-const fs = require('fs');
-const path = require('path');
-
 
 const plugins = [
   withImages,
@@ -28,13 +16,5 @@ const plugins = [
   ],
 ];
 
-// Other config goes here
-// const config = {
-//   reactStrictMode: true,
-//   lessVarsFilePath: './ant-theme.less',
-// };
-
 
 module.exports = withPlugins(plugins, {reactStrictMode: false})
-
-// module.exports = plugins.reduce((conf, plug) => plug(conf), config);
