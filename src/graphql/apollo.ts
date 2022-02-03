69c072e6d45ab92fa5fb60e5b99ea29e9ec4f1e4
Kevin Rodriguez <_@kevinrodriguez.io>
2022-02-02 20:29:16 -0600

    feat: moved to env vars

--- src/graphql/apollo.ts
+++ src/graphql/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const apolloClient = new ApolloClient({
-  uri: 'http://localhost:4000/v0', // TODO: Pull from env var,
+  uri: process.env.NEXT_PUBLIC_INDEXER_RPC_URL, // TODO: Pull from env var,
   cache: new InMemoryCache(),
 });
