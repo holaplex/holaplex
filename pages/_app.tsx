8da05338897c3b22e9b22f98ac295a79e2f0f289
Kristian Elset Boe <kristian.e.boe@gmail.com>
2022-03-08 19:55:37 +0100

    fix: update ukraine banner text and link

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -134,9 +134,9 @@ function MyApp({ Component, pageProps }: AppProps) {
                             <AnalyticsProvider>
                               <AppLayout>
                                 <div className='bg-[#005BBB] sm:flex items-center justify-center text-[#FFD500] p-6 w-full'>
-                                  Help the people of Ukraine through charity auctions.
+                                  Help the people of Ukraine through SOL donations.
                                   <a
-                                    href="https://twitter.com/the_urs_/status/1500248930436665352?s=21"
+                                    href="https://donate.metaplex.com/"
                                     className='sm:flex inline sm:h-10 sm:px-6 underline sm:no-underline ml-4 sm:rounded-full sm:hover:scale-[1.02] transition-transform sm:hover:text-[#005BBB] items-center justify-center sm:text-[#005BBB] sm:bg-[#FFD500]'
                                     target="_blank" 
                                     rel="noreferrer"
