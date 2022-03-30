7f933a3492aa1aec2d2a34f9c78aeb3d4bbf05d9
Kristian Elset Boe <kristian.e.boe@gmail.com>
2022-03-30 16:18:52 +0200

    fix: updated meta implementation

--- pages/_document.js
+++ pages/_document.js
@@ -59,36 +59,17 @@ export default class MyDocument extends Document {
             </>
           )}
           {META_ID && (
-            <>
-              <scipt
-                dangerouslySetInnerHTML={{
-                  __html: `
-                !function(f,b,e,v,n,t,s)
-                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                n.queue=[];t=b.createElement(e);t.async=!0;
-                t.src=v;s=b.getElementsByTagName(e)[0];
-                s.parentNode.insertBefore(t,s)}(window, document,'script',
-                'https://connect.facebook.net/en_US/fbevents.js');
-                fbq('init', '${META_ID}');
-                fbq('track', 'PageView');  
-                `,
+            <noscript>
+              <img
+                height="1"
+                width="1"
+                alt="t"
+                style={{
+                  display: 'none',
                 }}
+                src={`https://www.facebook.com/tr?id=${META_ID}&ev=PageView&noscript=1`}
               />
-
-              <noscript>
-                <img
-                  height="1"
-                  width="1"
-                  alt="t"
-                  style={{
-                    display: 'none',
-                  }}
-                  src={`https://www.facebook.com/tr?id=${META_ID}&ev=PageView&noscript=1`}
-                />
-              </noscript>
-            </>
+            </noscript>
           )}
         </Head>
         <body>
