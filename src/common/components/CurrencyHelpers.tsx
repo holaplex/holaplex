0bd94791a74a9ed4974a4cdb67b61fd13d03e2bd
Milo Rue <milorue@gmail.com>
2022-04-06 23:58:34 -0700

    improvement: currency helper variants

--- src/common/components/CurrencyHelpers.tsx
+++ src/common/components/CurrencyHelpers.tsx
@@ -4,11 +4,22 @@ import { SolIcon } from './elements/Price';
 
 interface DisplaySOLProps extends HTMLAttributes<HTMLParagraphElement> {
   amount: number;
+  iconVariant?: `small` | `large`;
 }
 
-export const DisplaySOL: FC<DisplaySOLProps> = ({ amount, className, ...rest }) =>
+const iconVariants = {
+  small: <SolIcon className="mr-1 h-3 w-3" stroke={`grey`} />,
+  large: <SolIcon className="mr-2 h-5 w-5" stroke={`grey`} />,
+};
+
+export const DisplaySOL: FC<DisplaySOLProps> = ({
+  amount,
+  className,
+  iconVariant = `large`,
+  ...rest
+}) =>
   amount ? (
     <b className={` inline-flex items-center ${className}`} {...rest}>
-      <SolIcon className="mr-2 h-5 w-5" stroke={`grey`} /> {amount / LAMPORTS_PER_SOL}
+      {iconVariants[iconVariant]} {amount / LAMPORTS_PER_SOL}
     </b>
   ) : null;
