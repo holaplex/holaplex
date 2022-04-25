31679a88e1b642ec4429f2777abc094365fa5d79
Austin Milt <austin@holaplex.com>
2022-04-25 17:24:00 -0400

    use carousel to display marketplace preview

--- src/common/components/home/FeaturedMarketplacesSection.tsx
+++ src/common/components/home/FeaturedMarketplacesSection.tsx
@@ -1,7 +1,8 @@
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/outline';
-import { MouseEventHandler, MutableRefObject, UIEvent, UIEventHandler, useEffect, useRef, useState, VFC } from 'react';
+import { VFC } from 'react';
 import HomeSection from './HomeSection';
 import MarketplacePreview from './MarketplacePreview';
+import Carousel from 'react-grid-carousel';
 
 const previewSubdomains: string[] = [
   'junglecats',
@@ -15,41 +16,23 @@ const previewSubdomains: string[] = [
   'junglecats',
 ];
 
+const PageLeftButton = (
+  <button
+    className="absolute left-0 top-1/2 h-10 w-10 -translate-x-1/2 -translate-y-1/2 rounded-full bg-gray-900 stroke-white p-1 shadow shadow-black hover:scale-105"
+  >
+    <ChevronLeftIcon />
+  </button>
+);
+
+const PageRightButton = (
+  <button
+    className="absolute right-0 top-1/2 h-10 w-10 translate-x-1/2 -translate-y-1/2 rounded-full bg-gray-900 stroke-white p-1 shadow shadow-black hover:scale-105"
+  >
+    <ChevronRightIcon />
+  </button>
+);
+
 const FeaturedMarkeplacesSection: VFC = () => {
-  const [canScrollLeft, setCanScrollLeft] = useState(false);
-  const [canScrollRight, setCanScrollRight] = useState(false);
-  const previewsContainer: MutableRefObject<HTMLDivElement | null> = useRef<HTMLDivElement>(null);
-
-  const onClickScrollLeftButton: MouseEventHandler<HTMLButtonElement> = () => {
-    if (canScrollLeft && previewsContainer.current) {
-      previewsContainer.current.scrollBy(-200, 0);
-      
-    }
-  }
-
-  const onClickScrollRightButton: MouseEventHandler<HTMLButtonElement> = () => {
-    if (canScrollRight && previewsContainer.current) {
-      previewsContainer.current.scrollBy(200, 0);
-    }
-  }
-
-  const updateFromScrolling: () => void = () => {
-    if (previewsContainer.current) {
-      const scrollDistanceFromLeft: number = previewsContainer.current.scrollLeft;
-      if (scrollDistanceFromLeft == 0) setCanScrollLeft(false);
-      else setCanScrollLeft(true);
-  
-      const scrollDistanceFromRight: number =
-        1 - (scrollDistanceFromLeft + previewsContainer.current.offsetWidth) / previewsContainer.current.scrollWidth;
-      if (scrollDistanceFromRight == 0) setCanScrollRight(false);
-      else setCanScrollRight(true);
-    }
-  }
-
-
-  useEffect(updateFromScrolling);
-
-
   return (
     <HomeSection>
       <HomeSection.Header>
@@ -59,35 +42,21 @@ const FeaturedMarkeplacesSection: VFC = () => {
         </HomeSection.HeaderAction>
       </HomeSection.Header>
       <HomeSection.Body>
-        <div className="relative">
-          <div
-            ref={previewsContainer}
-            onScroll={updateFromScrolling}
-            className="grid grid-flow-col grid-rows-2 gap-6 overflow-x-scroll p-4 md:gap-8"
-          >
-            {previewSubdomains.map((s) => (
-              <div key={s} className="aspect-[16/10] min-h-[194px] w-full md:min-h-[300px]">
+        <Carousel
+          cols={2}
+          rows={2}
+          gap={10}
+          arrowLeft={PageLeftButton}
+          arrowRight={PageRightButton}
+        >
+          {previewSubdomains.map((s) => (
+            <Carousel.Item key={s}>
+              <div key={s} className="aspect-[16/10] w-full p-4 overflow:visible">
                 <MarketplacePreview subdomain={s} />
               </div>
-            ))}
-          </div>
-          <button
-            onClick={onClickScrollLeftButton}
-            className={`${
-              canScrollLeft ? '' : 'hidden'
-            } absolute left-0 top-1/2 h-10 w-10 -translate-x-1/2 -translate-y-1/2 rounded-full bg-gray-900 stroke-white p-1 shadow shadow-black hover:scale-105`}
-          >
-            <ChevronLeftIcon />
-          </button>
-          <button
-            onClick={onClickScrollRightButton}
-            className={`${
-              canScrollRight ? '' : 'hidden'
-            } absolute right-0 top-1/2 h-10 w-10 translate-x-1/2 -translate-y-1/2 rounded-full bg-gray-900 stroke-white p-1 shadow shadow-black hover:scale-105`}
-          >
-            <ChevronRightIcon />
-          </button>
-        </div>
+            </Carousel.Item>
+          ))}
+        </Carousel>
       </HomeSection.Body>
     </HomeSection>
   );
