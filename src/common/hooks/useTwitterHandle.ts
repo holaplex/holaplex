180aacd6452537dd189056175ea532833b8bb9b1
Daniel <danieladammiller@gmail.com>
2022-04-27 15:23:02 -0700

    add support for cardinal

--- src/common/hooks/useTwitterHandle.ts
+++ src/common/hooks/useTwitterHandle.ts
@@ -4,15 +4,26 @@ import {
   getNameAccountKey,
   NameRegistryState,
 } from '@solana/spl-name-service';
+import { tryGetName } from '@cardinal/namespaces'
 import { useConnection } from '@solana/wallet-adapter-react';
 
 import { Connection, PublicKey } from '@solana/web3.js';
 import { useQuery } from 'react-query';
 
 export const getTwitterHandle = async (pk: string, connection: Connection) => {
+
   try {
-    const [twitterHandle] = await getHandleAndRegistryKey(connection, new PublicKey(pk));
-    return `${twitterHandle}`;
+    let thCardinal = await tryGetName(connection, new PublicKey(pk));
+    if (thCardinal) {
+      return `${thCardinal.replace('@', '')}`;
+    }
+    
+    let [twitterHandle] = await getHandleAndRegistryKey(connection, new PublicKey(pk));
+    if (twitterHandle) {
+      return `${twitterHandle}`;
+    }
+
+
   } catch (err: any) {
     return undefined;
   }
