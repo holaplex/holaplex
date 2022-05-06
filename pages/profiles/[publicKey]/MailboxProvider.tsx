84d1490396bc8fd23f74aabb57a8cd77bec65f47
viksit <viksit@usedispatch.xyz>
2022-05-06 21:30:45 +0000

    Add mailbox provider

---
+++ pages/profiles/[publicKey]/MailboxProvider.tsx
@@ -0,0 +1,38 @@
+import { createContext, useContext, useState, useEffect } from "react";
+import { useWallet } from "@solana/wallet-adapter-react";
+import { Mailbox } from '@usedispatch/client';
+import { useConnection } from '@solana/wallet-adapter-react';
+
+const MailboxContext = createContext<Mailbox | undefined>(undefined);
+
+type MailboxProviderProps = {
+  children: React.ReactNode;
+}
+
+export const MailboxProvider = (props: MailboxProviderProps): JSX.Element => {
+
+  const wallet = useWallet();
+
+  const { connection } = useConnection();
+  const [mailbox, setMailbox] = useState<Mailbox>();
+  const cluster = 'mainnet-beta';
+  //console.log('mbp', wallet, connection, cluster);
+
+  useEffect(() => {
+    if (!wallet.publicKey) {
+      return;
+    }
+    //console.log('1');
+    const mb = new Mailbox(connection, wallet, { cluster });
+    setMailbox(mb);
+    //console.log('mailbox: ', mb);
+  }, [connection, wallet.publicKey, cluster]);
+
+  return (
+    <MailboxContext.Provider value={mailbox}>
+      {props.children}
+    </MailboxContext.Provider>
+  );
+}
+
+export const useMailbox = () => useContext(MailboxContext);
