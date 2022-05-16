7e668737a3cfd56b81fc4c217817eddc7fa80a31
Milo Rue <milorue@gmail.com>
2022-05-16 11:13:25 -0700

    feat: moved file categories to a component

--- pages/nfts/[address].tsx
+++ pages/nfts/[address].tsx
@@ -43,6 +43,7 @@ import { seededRandomBetween } from '../../src/modules/utils/random';
 import { SolscanIcon } from '../../src/common/components/icons/Solscan';
 import { ExplorerIcon } from '../../src/common/components/icons/Explorer';
 import NFTImage from '../../src/common/components/elements/NFTImage';
+import NFTFile from '../../src/common/components/elements/NFTFile';
 
 const Activities = ({
   listings,
@@ -272,51 +273,7 @@ export default function NftByAddress({
                   </>
                 )}
               </div>
-              <div className="relative aspect-square w-full  ">
-                {(loading || !imgLoaded) && (
-                  <LoadingContainer className="absolute inset-0 rounded-lg bg-gray-800 shadow " />
-                )}
-                {nft?.category === `vr` ? (
-                  <div
-                    className={`block aspect-square w-full rounded-lg border-none object-cover shadow`}
-                  >
-                    {/* @ts-ignore */}
-                    <model-viewer
-                      style={{ width: `100%`, height: `100%` }}
-                      onLoad={() => setImgLoaded(true)}
-                      camera-controls
-                      alt={nft.name}
-                      src={
-                        nft.files.find((file) => file.fileType === `application/octet-stream`)?.uri
-                      }
-                    />
-                  </div>
-                ) : nft?.category === `video` || nft?.category === `audio` ? (
-                  <video
-                    onLoadStart={() => setImgLoaded(true)}
-                    onLoad={() => setImgLoaded(true)}
-                    className={`block aspect-square w-full rounded-lg border-none object-cover shadow`}
-                    playsInline={true}
-                    autoPlay={true}
-                    muted={true}
-                    controls={true}
-                    controlsList={`nodownload`}
-                    loop={true}
-                    poster={imgOpt(nft?.image, 800)!}
-                    src={nft.files[0].uri}
-                  />
-                ) : (
-                  nft?.image && (
-                    <NFTImage
-                      onLoad={() => setImgLoaded(true)}
-                      src={imgOpt(nft?.image, 800)!}
-                      className="block aspect-square w-full rounded-lg border-none object-cover shadow"
-                      alt=""
-                    />
-                  )
-                )}
-                {}
-              </div>
+              <NFTFile loading={loading} nft={nft as Nft | any} />
             </div>
             <div>
               <div className="mb-8 hidden lg:block">
