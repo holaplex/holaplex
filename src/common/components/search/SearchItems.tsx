6a2ffdee3a1c64f8655a6e4cef3177416c86b073
Milo Rue <milorue@gmail.com>
2022-05-18 11:52:17 -0700

    wip: manual verification

--- src/common/components/search/SearchItems.tsx
+++ src/common/components/search/SearchItems.tsx
@@ -1,7 +1,9 @@
 import React, { FC } from 'react';
-import { Avatar } from '../elements/Avatar';
+import { Avatar, AvatarImage } from '../elements/Avatar';
 import { shortenAddress } from '@/modules/utils/string';
 import Link from 'next/link';
+import { VERIFIED_COLLECTIONS } from '../../constants/search';
+import { Verified } from '../icons/Verified';
 
 interface ProfileSearchItemProps {
   address: string;
@@ -14,6 +16,33 @@ export const ProfileSearchItem: FC<ProfileSearchItemProps> = ({
   handle,
   profileImage,
 }) => {
+  const verifiedProfile = VERIFIED_COLLECTIONS.find((profile) => profile.address === address);
+  if (verifiedProfile) {
+    return (
+      <Link href={`/profiles/${address}`}>
+        <a
+          className={`flex flex-row items-center justify-between rounded-lg p-4 hover:bg-gray-800`}
+        >
+          <div className={`flex flex-row items-center gap-6`}>
+            <div className={`h-10 w-10`}>
+              <AvatarImage src={verifiedProfile.profileImage} />
+            </div>
+
+            <p className={`m-0 flex flex-row items-center gap-2 text-sm`}>
+              {verifiedProfile.profile}{' '}
+              <span className={`text-white`}>
+                <Verified />
+              </span>
+            </p>
+          </div>
+          <p className={`m-0 hidden text-sm text-gray-300 md:inline-block`}>
+            {shortenAddress(address)}
+          </p>
+        </a>
+      </Link>
+    );
+  }
+
   return (
     <Link href={`/profiles/${address}`}>
       <a className={`flex flex-row items-center justify-between rounded-lg p-4 hover:bg-gray-800`}>
@@ -44,6 +73,40 @@ export const NFTSearchItem: FC<NFTSearchItemProps> = ({
   address,
   creatorHandle,
 }) => {
+  const verifiedProfile = VERIFIED_COLLECTIONS.find(
+    (profile) => profile.address === creatorAddress
+  );
+  if (verifiedProfile) {
+    return (
+      <Link href={`/nfts/${address}`}>
+        <a
+          className={`flex flex-row items-center justify-between rounded-lg p-4 hover:bg-gray-800`}
+        >
+          <div className={`flex flex-row items-center gap-6`}>
+            <img
+              src={image}
+              alt={name}
+              className={`aspect-square h-10 w-10 overflow-hidden rounded-lg text-sm`}
+            />
+            <p className={`m-0 flex flex-row items-center gap-2 text-sm text-sm`}>
+              {name}{' '}
+              <span className={`text-white`}>
+                <Verified />
+              </span>
+            </p>
+          </div>
+          <p className={`m-0 hidden text-sm text-gray-300 md:inline-block`}>
+            {creatorHandle
+              ? `@${creatorHandle}`
+              : creatorAddress
+              ? shortenAddress(creatorAddress)
+              : null}
+          </p>
+        </a>
+      </Link>
+    );
+  }
+
   return (
     <Link href={`/nfts/${address}`}>
       <a className={`flex flex-row items-center justify-between rounded-lg p-4 hover:bg-gray-800`}>
@@ -56,7 +119,11 @@ export const NFTSearchItem: FC<NFTSearchItemProps> = ({
           <p className={`m-0 text-sm`}>{name}</p>
         </div>
         <p className={`m-0 hidden text-sm text-gray-300 md:inline-block`}>
-          {creatorHandle ? `@${creatorHandle}` : creatorAddress ? shortenAddress(creatorAddress) : null}
+          {creatorHandle
+            ? `@${creatorHandle}`
+            : creatorAddress
+            ? shortenAddress(creatorAddress)
+            : null}
         </p>
       </a>
     </Link>
---
+++ src/common/constants/search.ts
@@ -0,0 +1,10 @@
+// TODO: temporary manually verify collections
+export const VERIFIED_COLLECTIONS = [
+  {
+    prefix: 'Okay Bear',
+    address: '3xVDoLaecZwXXtN59o6T3Gfxwjcgf8Hc9RfoqBn995P9',
+    profile: 'Okay Bears',
+    profileImage:
+      'https://bafkreidgfsdjx4nt4vctch73hcchb3pkiwic2onfw5yr4756adchogk5de.ipfs.dweb.link/',
+  },
+];
