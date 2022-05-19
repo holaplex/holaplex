ea62aed1f0e1c2661fa5ab3ea92df42ccec0c294
calebbabin <object.caleb@gmail.com>
2022-05-19 15:10:17 -0500

    update investors stub

--- src/common/assets/investors/investors-stub.ts
+++ src/common/assets/investors/investors-stub.ts
@@ -1,35 +1,39 @@
-import CoinfundLogo from '@/assets/investors/logos/test.svg';
-
 const stores = [
   {
     name: 'Coinfund',
     url: 'https://www.coinfund.io',
     logo: '/images/investor-logos/coin-fund.png',
+    logoLight: '/images/investor-logos/coin-fund-light.png',
   },
   {
     name: 'Solana',
     url: 'https://solana.com',
     logo: '/images/investor-logos/solana.png',
+    logoLight: '/images/investor-logos/solana-light.png',
   },
   {
     name: 'Valor',
     url: 'https://valor.vc',
     logo: '/images/investor-logos/valor.png',
+    logoLight: '/images/investor-logos/valor-light.png',
   },
   {
     name: 'Collab + Currency',
     url: 'https://www.collabcurrency.com',
     logo: '/images/investor-logos/collab-currency.png',
+    logoLight: '/images/investor-logos/collab-currency-light.png',
   },
   {
     name: 'Social Capital',
     url: 'https://www.socialcapital.com',
     logo: '/images/investor-logos/social-capital.png',
+    logoLight: '/images/investor-logos/social-capital-light.png',
   },
   {
     name: 'Lemnis Capital',
     url: 'https://lemniscap.com',
     logo: '/images/investor-logos/lemnis.png',
+    logoLight: '/images/investor-logos/lemnis-light.png',
   },
   {
     name: 'Blockchain Coinvest',
@@ -40,11 +44,13 @@ const stores = [
     name: 'Hard Yaka',
     url: 'https://hardyaka.com',
     logo: '/images/investor-logos/hard-yaka.png',
+    logoLight: '/images/investor-logos/hard-yaka-light.png',
   },
   {
     name: 'Kano One',
     url: 'https://kano.one',
     logo: '/images/investor-logos/kano.png',
+    logoLight: '/images/investor-logos/kano-light.png',
   },
   {
     name: 'Filecoin',
@@ -56,6 +62,7 @@ const stores = [
     name: 'Arweave',
     url: 'https://arweave.org',
     logo: '/images/investor-logos/arweave.png',
+    logoLight: '/images/investor-logos/arweave-light.png',
   },
 ];
 
