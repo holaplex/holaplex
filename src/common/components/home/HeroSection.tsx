7c694f965d1618611bd391086df9ceeff3035f42
Austin Milt <austin@holaplex.com>
2022-05-20 12:51:19 -0600

    fix loading state of hero cards

--- src/common/components/home/HeroSection.tsx
+++ src/common/components/home/HeroSection.tsx
@@ -1,16 +1,18 @@
 import { ArrowRightIcon } from '@heroicons/react/outline';
 import Link from 'next/link';
-import React from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import { useFeedQuery } from 'src/graphql/indexerTypes';
 import { Button5 } from '../elements/Button2';
 import { FeedQueryEvent } from '../feed/feed.utils';
 import { FeedCard, LoadingFeedCard } from '../feed/FeedCard';
 import Marquee from 'react-fast-marquee';
+import classNames from 'classnames';
 
 const ALPHA_WALLET = 'NWswq7QR7E1i1jkdkddHQUFtRPihqBmJ7MfnMCcUf4H';
+const N_ITEMS = 12;
 
 export default function HeroSection() {
-  const { data } = useFeedQuery({
+  const { data, loading } = useFeedQuery({
     variables: {
       address: ALPHA_WALLET,
       offset: 0,
@@ -18,41 +20,46 @@ export default function HeroSection() {
     },
   });
   const feedEvents = data?.feedEvents || [];
-  const feedItems: FeedQueryEvent[] = [];
+  const feedItems: FeedQueryEvent[] = useMemo(() => [], []);
+
+  // hack to prevent the marquee from appearing to resize over multiple rows once the feed cards are loaded
+  const [showLoadingCards, setShowLoadingCards] = useState<boolean>(true);
+  useEffect(() => {
+    setTimeout(() => setShowLoadingCards(feedItems.length == 0), 100);
+  }, [loading, feedItems]);
 
   feedEvents.forEach((event) => {
     if (
       event.__typename !== 'FollowEvent' &&
-      !feedItems.find((i) => i.feedEventId === event.feedEventId)
+      !feedItems.find((i) => i && i.feedEventId === event.feedEventId)
     ) {
       feedItems.push(event);
     }
   });
 
   return (
-    <div className="">
+    <div>
       <Marquee speed={40} gradient={false} pauseOnHover={true}>
-        <div className="grid grid-flow-col gap-8 overflow-x-scroll py-2 pl-8 no-scrollbar  ">
-          {!feedItems.length
-            ? Array(12)
-                .fill(null)
-                .map((_, i) => (
-                  <div key={i} className="w-96">
-                    <LoadingFeedCard />
-                  </div>
-                ))
-            : feedItems.slice(0, 12).map((fi) => (
-                <div className="w-96 flex-shrink-0" key={fi.feedEventId}>
-                  <FeedCard
-                    options={{ hideAction: true }}
-                    event={fi}
-                    myFollowingList={[]}
-                    key={fi.feedEventId}
-                  />
-                </div>
-              ))}
+        <div className={classNames("grid grid-flow-col gap-8 overflow-x-scroll py-2 pl-8 no-scrollbar", {hidden: showLoadingCards})}>
+          {feedItems.slice(0, N_ITEMS).map((fi, i) => (
+            <div className="w-96 flex-shrink-0" key={fi.feedEventId}>
+              <FeedCard
+                  options={{ hideAction: true }}
+                  event={fi}
+                  myFollowingList={[]}
+                  key={fi.feedEventId}
+                />
+            </div>
+          ))}
         </div>
       </Marquee>
+      <div className={classNames("grid grid-flow-col gap-8 overflow-x-scroll py-2 pl-8 no-scrollbar", {hidden: !showLoadingCards})}>
+        {Array(N_ITEMS).fill(null).map((_, i) => (
+          <div className='w-96 flex-shrink-0' key={i}>
+            <LoadingFeedCard />
+          </div>
+        ))}
+      </div>
 
       <div className="mt-20 flex flex-col items-center text-center">
         <h1 className="text-3xl font-medium">
