9462993b9fe971ab0a143b91a228f63e4c3d109c
Austin Milt <austin@holaplex.com>
2022-05-23 13:00:18 -0600

    move arrows on carousel

--- pages/index.tsx
+++ pages/index.tsx
@@ -52,13 +52,13 @@ const ExternalLink: FC<HomeLinkProps> = ({ href, children }) => (
 );
 
 const PageLeftButton = (
-  <button className="absolute left-2 top-1/2 z-50 flex h-10 w-10 -translate-y-1/2 items-center justify-center rounded-full bg-gray-900 stroke-white p-1 shadow shadow-black transition hover:scale-125">
+  <button className="absolute left-4 top-1/2 z-50 flex h-10 w-10 -translate-y-1/2 items-center justify-center rounded-full bg-gray-900 stroke-white p-1 shadow shadow-black transition hover:scale-125">
     <ChevronLeftIcon className="h-4 w-4" />
   </button>
 );
 
 const PageRightButton = (
-  <button className="absolute right-2 top-1/2 z-50 flex h-10 w-10 -translate-y-1/2 items-center justify-center rounded-full bg-gray-900 stroke-white p-1 shadow shadow-black transition hover:scale-125">
+  <button className="absolute right-4 top-1/2 z-50 flex h-10 w-10 -translate-y-1/2 items-center justify-center rounded-full bg-gray-900 stroke-white p-1 shadow shadow-black transition hover:scale-125">
     <ChevronRightIcon className="h-4 w-4" />
   </button>
 );
