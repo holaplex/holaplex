97a6fc27cb75c6e3e7aa1a5c7b5f007d68b9dc05
calebbabin <object.caleb@gmail.com>
2022-05-24 11:34:39 -0500

    reorganize about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -144,132 +144,22 @@ export default function About() {
           content="Our mission is to empower creators and collectors with a suite of tools to create, market, and sell NFTs."
         />
       </Head>
-      <HeroSection>
-        <Image
-          src={'/images/page-about/hero.jpg'}
-          objectFit="cover"
-          layout="fill"
-          quality={100}
-          alt=""
-        />
-        <div className="relative inset-y-10 right-0 ml-auto h-64 w-10/12 lg:absolute lg:h-auto lg:w-5/12 ">
-          <Image
-            src={'/images/page-about/laptop.png'}
-            alt="Marketplace preview"
-            layout={'fill'}
-            objectFit={'contain'}
-            objectPosition={'center right'}
-          />
-        </div>
-        <Content>
-          <div className="mt-12 lg:mt-0 lg:w-9/12">
-            <BoldParagraph className="mb-4 text-gray-400 lg:mb-14">POWERED BY Solana</BoldParagraph>
-            <GradientText>
-              Incredibly Fast, <br className="hidden lg:block" />
-              Low Cost &amp; Eco-Friendly.
-            </GradientText>
-            <BoldParagraph className="color-white mt-4 lg:mt-14">
-              Tools that are open source, owned by creators, are <br className="hidden lg:block" />{' '}
-              permissionless, and governed by the community.
-            </BoldParagraph>
-          </div>
-        </Content>
-      </HeroSection>
-
-      <Section className="bg-gray-800">
-        <Image src={'/images/page-about/connect.jpg'} alt="" objectFit="cover" layout="fill" />
-        <Content className="text-center">
-          <div className="py-8">
-            <Title className="mt-0">Connect Your Wallet</Title>
-            <Link href="/alpha">
-              <a>
-                <Button>Claim Your Profile</Button>
-              </a>
-            </Link>
-          </div>
-        </Content>
-      </Section>
 
       <Section className="bg-black">
         <Content className="text-center">
-          <BoldParagraph className="mb-8 text-gray-400 lg:mb-14">Our partners</BoldParagraph>
-          <GradientText>
-            Supported &amp; Trusted By <br className="hidden lg:block" /> The Best
-          </GradientText>
-          <BoldParagraph className="my-8 text-gray-500 lg:my-14">
-            We work with leading funds and platforms
+          <BoldParagraph className="mb-8 text-gray-400 lg:mb-14">YOUR COMMUNITY</BoldParagraph>
+          <GradientText>A community driven by you</GradientText>
+          <BoldParagraph className="color-white mx-auto mt-8 max-w-2xl lg:mt-14">
+            You created it and you own it. Our community believes in being radically open,
+            decentralized and permissionless.
           </BoldParagraph>
-          <div className="my-14 flex flex-wrap justify-center gap-4 lg:my-28 lg:gap-8">
-            {investorData.map((investor, index) => (
-              <LogoContainer
-                key={investor.url}
-                href={investor.url}
-                target="_blank"
-                rel="noreferrer"
-              >
-                <Image
-                  layout="fill"
-                  objectFit="contain"
-                  src={investor.logoLight || investor.logo}
-                  alt={investor.name}
-                />
-              </LogoContainer>
-            ))}
-          </div>
         </Content>
-      </Section>
-
-      <Section className="bg-black">
-        <Content>
-          <div className="mb-12 lg:mb-0 lg:w-3/4">
-            <BoldParagraph className="mb-8 text-gray-400 lg:mb-14">WHAT WE CREATE</BoldParagraph>
-            <GradientText>
-              Three Main Tools of <br className="hidden lg:block" /> the Platform
-            </GradientText>
-            <BoldParagraph className="color-white mt-8 lg:mt-14">
-              With Holaplex, it&apos;s easier than ever to get started in the NFT community.
-              We&apos;re committed to making it easy for everyone to join.
-            </BoldParagraph>
-          </div>
-          <div className="flex flex-wrap items-center justify-around gap-8 lg:hidden">
-            <MainTool>
-              <b>No-Code Tools</b>
-              <div>
-                <Image src={'/images/page-about/main-tool.svg'} width="94" height="94" />
-              </div>
-              <p>Profiles and Marketplaces</p>
-            </MainTool>
-            <MainTool className="lg:mt-16">
-              <b>Social Graph</b>
-              <div>
-                <Image src={'/images/page-about/main-tool.svg'} width="94" height="94" />
-              </div>
-              <p>Solana&apos;s NFT Social Network supports following and feed</p>
-            </MainTool>
-            <MainTool className="lg:-mt-24 xl:-mt-32">
-              <b>Indexer</b>
-              <div>
-                <Image src={'/images/page-about/main-tool.svg'} width="94" height="94" />
-              </div>
-              <p>Read Sonlana&apos;s on-chain NFT related data fast and efficiently</p>
-            </MainTool>
-          </div>
-        </Content>
-        <div 
-          className='hidden lg:block w-full overflow-hidden pointer-events-none' 
-          style={{ marginTop: 'clamp(-500px, -12%, 0px)' }}
-         >
-          <div 
-            className='max-w-8xl flex justify-center' 
-            style={{ width: '120%', marginLeft: '-12%' }}
-           >
-            <Image 
-              src={'/images/page-about/three-main-tools.svg'} 
-              width="2994" 
-              height="1417" 
-              className='w-full'
-              alt=""
-             />
+        <div className='max-w-6xl w-10/12 mx-auto'>
+          <div className='flex flex-wrap justify-around items-center mt-24 lg:mt-48'>
+            <CommunityNFTContainer style={{ animationDelay: '250ms ', animationDuration: '5s' }} className=''><Image src={'/images/page-about/nft-1.jpg'} width="500" height="500" alt="" /></CommunityNFTContainer>
+            <CommunityNFTContainer style={{ animationDelay: '500ms ', animationDuration: '3s' }} className='z-20'><Image src={'/images/page-about/nft-2.jpg'} width="500" height="500" alt="" /></CommunityNFTContainer>
+            <CommunityNFTContainer style={{ animationDelay: '1500ms', animationDuration: '5s' }} className='z-10'><Image src={'/images/page-about/nft-3.jpg'} width="500" height="500" alt="" /></CommunityNFTContainer>
+            <CommunityNFTContainer style={{ animationDelay: '2000ms', animationDuration: '6s' }} className=''><Image src={'/images/page-about/nft-4.jpg'} width="500" height="500" alt="" /></CommunityNFTContainer>
           </div>
         </div>
       </Section>
@@ -324,39 +214,91 @@ export default function About() {
         </SlantedBgContainer>
       </div>
 
+
       <Section className="bg-black">
         <Content className="text-center">
-          <BoldParagraph className="mb-8 text-gray-400 lg:mb-14">YOUR COMMUNITY</BoldParagraph>
-          <GradientText>A community driven by you</GradientText>
-          <BoldParagraph className="color-white mx-auto mt-8 max-w-2xl lg:mt-14">
-            You created it and you own it. Our community believes in being radically open,
-            decentralized and permissionless.
+          <BoldParagraph className="mb-8 text-gray-400 lg:mb-14">Our partners</BoldParagraph>
+          <GradientText>
+            Supported &amp; Trusted By <br className="hidden lg:block" /> The Best
+          </GradientText>
+          <BoldParagraph className="my-8 text-gray-500 lg:my-14">
+            We work with leading funds and platforms
           </BoldParagraph>
+          <div className="my-14 flex flex-wrap justify-center gap-4 lg:my-28 lg:gap-8">
+            {investorData.map((investor, index) => (
+              <LogoContainer
+                key={investor.url}
+                href={investor.url}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Image
+                  layout="fill"
+                  objectFit="contain"
+                  src={investor.logoLight || investor.logo}
+                  alt={investor.name}
+                />
+              </LogoContainer>
+            ))}
+          </div>
         </Content>
-        <div className='max-w-6xl w-10/12 mx-auto'>
-          <div className='flex flex-wrap justify-around items-center mt-24 lg:mt-48'>
-            <CommunityNFTContainer style={{ animationDelay: '250ms ', animationDuration: '5s' }} className=''><Image src={'/images/page-about/nft-1.jpg'} width="500" height="500" alt="" /></CommunityNFTContainer>
-            <CommunityNFTContainer style={{ animationDelay: '500ms ', animationDuration: '3s' }} className='z-20'><Image src={'/images/page-about/nft-2.jpg'} width="500" height="500" alt="" /></CommunityNFTContainer>
-            <CommunityNFTContainer style={{ animationDelay: '1500ms', animationDuration: '5s' }} className='z-10'><Image src={'/images/page-about/nft-3.jpg'} width="500" height="500" alt="" /></CommunityNFTContainer>
-            <CommunityNFTContainer style={{ animationDelay: '2000ms', animationDuration: '6s' }} className=''><Image src={'/images/page-about/nft-4.jpg'} width="500" height="500" alt="" /></CommunityNFTContainer>
+      </Section>
+
+      <Section className="bg-black" style={{ paddingBottom: '0px' }}>
+        <Content>
+          <div className="mb-12 lg:mb-0 lg:w-3/4">
+            <BoldParagraph className="mb-8 text-gray-400 lg:mb-14">WHAT WE CREATE</BoldParagraph>
+            <GradientText>
+              Three Main Tools of <br className="hidden lg:block" /> the Platform
+            </GradientText>
+            <BoldParagraph className="color-white mt-8 lg:mt-14">
+              With Holaplex, it&apos;s easier than ever to get started in the NFT community.
+              We&apos;re committed to making it easy for everyone to join.
+            </BoldParagraph>
+          </div>
+          <div className="flex flex-wrap items-center justify-around gap-8 lg:hidden">
+            <MainTool>
+              <b>No-Code Tools</b>
+              <div>
+                <Image src={'/images/page-about/main-tool.svg'} width="94" height="94" />
+              </div>
+              <p>Profiles and Marketplaces</p>
+            </MainTool>
+            <MainTool className="lg:mt-16">
+              <b>Social Graph</b>
+              <div>
+                <Image src={'/images/page-about/main-tool.svg'} width="94" height="94" />
+              </div>
+              <p>Solana&apos;s NFT Social Network supports following and feed</p>
+            </MainTool>
+            <MainTool className="lg:-mt-24 xl:-mt-32">
+              <b>Indexer</b>
+              <div>
+                <Image src={'/images/page-about/main-tool.svg'} width="94" height="94" />
+              </div>
+              <p>Read Sonlana&apos;s on-chain NFT related data fast and efficiently</p>
+            </MainTool>
+          </div>
+        </Content>
+        <div
+          className='hidden lg:block w-full overflow-hidden pointer-events-none'
+          style={{ marginTop: 'clamp(-500px, -12%, 0px)' }}
+        >
+          <div
+            className='max-w-8xl flex justify-center'
+            style={{ width: '120%', marginLeft: '-12%' }}
+          >
+            <Image
+              src={'/images/page-about/three-main-tools.svg'}
+              width="2994"
+              height="1417"
+              className='w-full'
+              alt=""
+            />
           </div>
         </div>
       </Section>
-
-      {/*
-        <Section>
-          <Content className='text-center'>
-            <GradientText>
-              GitHub Projects
-            </GradientText>
-            <BoldParagraph className='mt-14 color-gray-400'>
-              Open-source tools increase velocity of innovation so we can win together. Come build with us!
-            </BoldParagraph>
-          </Content>
-        </Section>
-        */}
-
-      <div className="-mb-20 h-20 bg-black" />
+      <div className='-mt-20' />
       <Footer />
     </>
   );
