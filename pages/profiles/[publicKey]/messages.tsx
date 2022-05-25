f3cacfa16a7ac3bc4743abf56cf97303c2ef74b3
Jason Schorr <echohtp@gmail.com>
2022-05-25 14:08:40 -0400

    looking better

--- pages/profiles/[publicKey]/messages.tsx
+++ pages/profiles/[publicKey]/messages.tsx
@@ -8,59 +8,92 @@ import {
 import { ProfileDataProvider } from '@/common/context/ProfileData';
 import { ProfilePageHead } from '../[publicKey]';
 import { useMailbox } from './MailboxProvider';
-import { MessageAccount } from "@usedispatch/client";
+import { MessageAccount } from '@usedispatch/client';
 import { useEffect, useState } from 'react';
-import * as web3 from "@solana/web3.js";
+import * as web3 from '@solana/web3.js';
 
-export const getServerSideProps: GetServerSideProps<WalletDependantPageProps> = async (context) =>
+export const getServerSideProps: GetServerSideProps<WalletDependantPageProps> = async context =>
   getProfileServerSideProps(context);
 
 const MessagesPage: NextPage<WalletDependantPageProps> = ({ publicKey, ...props }) => {
-
-  const [messages, setMessages] = useState<MessageAccount[]>([]);
-  const [mailboxAddress, setMailboxAddress] = useState<web3.PublicKey | null>( null );
+  const [messages, setMessages] = useState<Map<string, []>>(new Map());
+  const [uniqueSenders, setUniqueSenders] = useState<string[]>([])
+  const [mailboxAddress, setMailboxAddress] = useState<web3.PublicKey | null>(null);
 
   const mailbox = useMailbox();
 
   useEffect(() => {
-
     // console.log('use effect');
     if (mailbox) {
       // console.log('mailbox!: ', mailbox);
-      mailbox
-        .getMailboxAddress()
-        .then((address) => setMailboxAddress(address));
+      mailbox.getMailboxAddress().then(address => setMailboxAddress(address));
 
       mailbox
         .fetchMessages()
-        .then((messages) => {
+        .then(messages => {
           console.log('messages>', messages);
-          setMessages(messages);
+
+          const uniqueSenders = [
+            ...new Set(messages.map((message: any) => message.sender.toBase58())),
+          ];
+          const pArray = uniqueSenders.map(u =>
+            mailbox.fetchSentMessagesTo(new web3.PublicKey(String(u)))
+          );
+          Promise.all(pArray).then(msgs => {
+            let allMessages = new Map();
+            for (var i = 0; i < uniqueSenders.length; i++) {
+              //@ts-ignore // we know they will be strings
+              allMessages[uniqueSenders[i]] = [
+                ...msgs[i],
+                ...messages.filter(
+                  (m: any) => m.receiver == publicKey && m.sender == uniqueSenders[i]
+                )]
+                .sort((a, b) => {
+                if (new Date(String(a.data.ts)).getTime() > new Date(String(b.data.ts)).getTime()){
+                  console.log("sort true")
+                  return -1
+                }
+                
+                if (new Date(String(a.data.ts)).getTime() < new Date(String(b.data.ts)).getTime()){
+                  console.log("sort false")
+                  return -1
+                }
+
+                return 0 
+              });
+            }
+            setMessages(allMessages);
+            setUniqueSenders(uniqueSenders)
+          });
         })
         .catch(() => {
-          setMessages([]);
+          setMessages(new Map());
         });
     } else {
       console.log('no mailbox initiatlized');
     }
-
   }, [mailbox]);
 
-      return (
-  <ProfileDataProvider profileData={{ publicKey, ...props }}>
-    <ProfilePageHead
-      publicKey={publicKey}
-      twitterProfile={{
-        twitterHandle: props.twitterHandle,
-        banner: props.banner,
-        pfp: props.profilePicture,
-      }}
-      description="View activity for this, or any other pubkey, in the Holaplex ecosystem."
-    />
-    <ProfileContainer>
-    <ProfileMessages publicKey={publicKey} messages={messages} mailboxAddress={mailboxAddress} mailbox={mailbox} />
-    </ProfileContainer>
-  </ProfileDataProvider>
-);
-    }
+  return (
+    <ProfileDataProvider profileData={{ publicKey, ...props }}>
+      <ProfilePageHead
+        publicKey={publicKey}
+        twitterProfile={{
+          twitterHandle: props.twitterHandle,
+          banner: props.banner,
+          pfp: props.profilePicture,
+        }}
+        description='View activity for this, or any other pubkey, in the Holaplex ecosystem.'
+      />
+
+      <ProfileMessages
+        publicKey={publicKey}
+        messages={messages}
+        mailboxAddress={mailboxAddress}
+        mailbox={mailbox}
+        uniqueSenders={uniqueSenders}
+      />
+    </ProfileDataProvider>
+  );
+};
 export default MessagesPage;
--- src/common/components/elements/ProfileMessages.tsx
+++ src/common/components/elements/ProfileMessages.tsx
@@ -5,7 +5,7 @@ import { useAnalytics } from '@/common/context/AnalyticsProvider';
 import { showFirstAndLastFour } from '@/modules/utils/string';
 import { AnchorWallet } from '@solana/wallet-adapter-react';
 import { Connection } from '@solana/web3.js';
-import React, { FC, useState, useEffect } from 'react';
+import React, { FC, useState, useEffect, useMemo } from 'react';
 import { useQueryClient } from 'react-query';
 import { toast } from 'react-toastify';
 import { Button5 } from './Button2';
@@ -17,55 +17,18 @@ import { Mailbox, MessageAccount } from '@usedispatch/client';
 interface ProfileMessagesInterface {
   publicKey: string;
   mailbox: Mailbox | undefined;
-  messages: any;
+  messages: Map<any, any>;
   mailboxAddress: web3.PublicKey | null;
   receiver?: any;
+  uniqueSenders: string[];
 }
 
 export const ProfileMessages = ({ publicKey, ...props }: ProfileMessagesInterface) => {
   const [tx, setTx] = useState<string>('');
   const [receiver, setReceiver] = useState<string>(props.receiver ?? '');
-  const { mailbox, messages, mailboxAddress } = props;
+  const { mailbox, messages, uniqueSenders, mailboxAddress } = props;
   const [selectedMessage, setSelectedMessage] = useState<string>('');
   // console.log(mailbox, messages, mailboxAddress);
-  const [allMessages, setAllMessages] = useState<Map<string, []>>(new Map());
-
-  const handleReceiverChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (props.receiver) {
-      setReceiver(props.receiver);
-    } else {
-      setReceiver(e.target.value);
-    }
-  };
-
-  useEffect(() => {
-    console.log('use effect profilemessages');
-    if (mailbox && messages) {
-      const uniqueSenders = [...new Set(messages.map((message: any) => message.sender.toBase58()))];
-      const pArray = uniqueSenders.map(u =>
-        mailbox.fetchSentMessagesTo(new web3.PublicKey(String(u)))
-      );
-      Promise.all(pArray).then(msgs => {
-        console.log(msgs);
-        // setSentMessages(msgs);
-
-        let allMessages = new Map();
-        for (var i = 0; i < uniqueSenders.length; i++) {
-          console.log('Sent to: ', uniqueSenders[i]);
-          console.log(msgs[i]);
-          //@ts-ignore // we know they will be strings
-          allMessages[uniqueSenders[i]] = [
-            ...msgs[i],
-            ...messages.filter((m: any) => m.receiver == publicKey && m.sender == uniqueSenders[i]),
-          //@ts-ignore
-          ].sort((a, b) => a.data.ts > b.data.ts);
-        }
-        console.log('all messages');
-        console.log(allMessages);
-        setAllMessages(allMessages);
-      });
-    }
-  }, [mailbox]);
 
   const sendMessage = async (event: React.SyntheticEvent) => {
     event.preventDefault();
@@ -111,16 +74,28 @@ export const ProfileMessages = ({ publicKey, ...props }: ProfileMessagesInterfac
       });
   };
 
-  const uniqueSenders = [...new Set(messages.map((message: any) => message.sender.toBase58()))];
 
+  useEffect(()=>{
+    if (uniqueSenders.length > 0){
+      // FETCH PROFILE INFO HERE
+    }
+  }, [uniqueSenders])
+  
   return (
-    <>
-      <div className='flex'>
-        <div className='flex-initial mr-4 border-r border-white w-52'>
+    <div className="container h-[85%]">
+      <div className='flex h-[85%]'>
+        <div className='flex-initial mr-4 border-r border-gray-600 w-72'>
           <div className='grid-flow-col'>
-            {uniqueSenders.map((us: any) => (
+            {uniqueSenders.map((us: any) => {
+              
+              let cx = ""
+              if (us == selectedMessage){
+                cx = "bg-gray-600"
+              }
+              
+              return (
               <div
-                className='px-2 py-2 mx-2 my-4 border border-white rounded-full'
+                className={'px-2 py-2 mx-2 my-4 rounded-md ' + cx}
                 onClick={() => {
                   setSelectedMessage(us);
                   setReceiver(us);
@@ -129,53 +104,41 @@ export const ProfileMessages = ({ publicKey, ...props }: ProfileMessagesInterfac
                 <img
                   width='40'
                   height='40'
-                  src='https://pbs.twimg.com/profile_images/1524044279664066562/CymWl0U4.jpg'
+                  src='https://pbs.twimg.com/profile_images/1529154555874795520/8lf1n7OZ_400x400.jpg'
                   className='inline-block rounded-full shadow-lg shadow-black'
                   alt='Profile Image'
                 />
-                <span className='inline-block'>{showFirstAndLastFour(us)}</span>
+                <span className='inline-block pl-2'>{showFirstAndLastFour(us)}</span>
               </div>
-            ))}
+            )})}
           </div>
-          <span>
-            <p>Selected: {showFirstAndLastFour(selectedMessage)}</p>
-          </span>
+          <div className='text-center'>
+            <button className='w-20 text-white bg-gray-500 rounded-lg h-17 hover:bg-gray-600'>New message</button>
+          </div>
         </div>
-        <div className='grow'>
-          <div className='grid grid-flow-row'>
+        <div className='mr-10 grow'>
+          <div className='grid grid-flow-row h-[80%]'>
             {//@ts-ignore
-            selectedMessage != '' && allMessages[selectedMessage] ? (
+            selectedMessage != '' && messages[selectedMessage] ? (
               //@ts-ignore
-              allMessages[selectedMessage].map((message: any) => {
-                let cx = '';
+              messages[selectedMessage].map((message: any) => {
+                let cx, dx= '';
                 if (message.sender.toBase58() == publicKey) {
-                  cx = 'background-blue text-white';
+                  cx = 'text-right';
+                  dx = 'float-right'
                 } else {
-                  cx = 'background-black text-white border border-white';
+                  cx = 'text-left';
                 }
 
                 return (
-                  // <div>
-                  //
-                  //   <br />
-                  //   Subj: {message.data.subj}
-                  //   <br />
-                  //   Body: {message.data.body}
-                  //   <br />
-                  //   TS: {message.data.ts?.toDateString()}
-                  //   <br />
-                  //   <div>
-                  //     <br />
-                  //   </div>
-                  // </div>
-                  <div className={'py-2 ' + cx}>
-                    <span>From:{showFirstAndLastFour(message.sender.toBase58())}</span>
-                    <br />
-                    <span>To:{showFirstAndLastFour(message.receiver.toBase58())}</span>
-                    <br />
-                    <span>{String(message.data.ts)}</span>
-                    <br />
-                    <span>{message.data.body}</span>
+                  <div className={'py-2 ' + cx }>
+                    <div className={'bg-gray-600 rounded-xl w-[33%] ' + dx}>
+                      <span>
+                        {message.data.body}
+                        <br />
+                        {String(message.data.ts.toDateString())}
+                    </span>
+                    </div>
                   </div>
                 );
               })
@@ -183,49 +146,27 @@ export const ProfileMessages = ({ publicKey, ...props }: ProfileMessagesInterfac
               <></>
             )}
           </div>
-          <form onSubmit={sendMessage} className='bottom-0'>
+          <form onSubmit={sendMessage} className='relative bottom-0 '>
             <div className='relative'>
               <input
                 type='text'
                 name='message'
                 className='z-0 w-full pl-10 pr-20 text-white bg-gray-800 rounded-lg h-14 focus:shadow focus:outline-none focus:border-0'
-                placeholder='Search anything...'
+                placeholder=''
               />
               <div className='absolute top-2 right-2'>
                 <button
-                  className='w-20 h-10 text-white bg-red-500 rounded-lg hover:bg-red-600'
+                  className='w-20 h-10 text-white bg-gray-500 rounded-lg hover:bg-gray-600'
                   type='submit'
                 >
                   Send
                 </button>
               </div>
             </div>
-            {/* <div className='form-control '>
-            <textarea
-              className='w-full h-24 text-black border-gray-300 textarea textarea-bordered'
-              placeholder='Message'
-              name='message'
-            />
-          </div>
-          <div className='flex justify-end pt-8'>
-            <button
-              className='text-white bg-gray-800 btn btn-primary hover:bg-gray-700 hover:text-white '
-              
-            >
-              {'Send message'}
-            </button>
-          </div> */}
+           
           </form>
         </div>
       </div>
-
-      {/*       <div className='border border-white'>
-        <h1>Compose</h1>
-
-        <h1>Messages go here</h1>
-        <h2>ADDRESS: {mailboxAddress?.toBase58() ?? ''}</h2>
-      </div>
-     */}
-    </>
+    </div>
   );
 };
