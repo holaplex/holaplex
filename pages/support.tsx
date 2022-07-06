cdaa74afabe949853fb2c191a30e5fae4124d8b8
calebbabin <object.caleb@gmail.com>
2022-07-06 16:35:57 -0500

    fix footer import

--- pages/support.tsx
+++ pages/support.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Head from 'next/head';
-import Footer from '@/common/components/home/Footer';
+import Footer from 'src/views/home/Footer';
 import Image from 'next/image';
 
 const Heading = styled.h2`
