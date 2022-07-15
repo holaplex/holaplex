e7a8c8ecf5af837e1917fa3ea52e760e90f9dcde
Daniel <danieladammiller@gmail.com>
2022-07-15 09:47:36 -0700

    dont run xmint code if no api key, more generous node version check

--- src/modules/crossmint/index.ts
+++ src/modules/crossmint/index.ts
@@ -6,28 +6,33 @@ const baseUrl = `https://www.crossmint.io/api/v1-alpha1`;
 const client = axios.create({ baseURL: baseUrl });
 
 export const verifyTOS = async (wallet: string) => {
-  const res = await client.get(`/tos/status?locator=sol:${wallet}`, {
-    headers: {
-      'x-api-key': crossmintConfig.apiKey,
-    },
-  });
-
-  return res;
-};
-
-export const acceptTOS = async (wallet: string) => {
-  const res = await client.post(
-    `/tos/accept`,
-    {
-      locator: `sol:${wallet}`,
-      marketplaceName: 'Holaplex',
-    },
-    {
+  if (crossmintConfig.apiKey) {
+    const res = await client.get(`/tos/status?locator=sol:${wallet}`, {
       headers: {
         'x-api-key': crossmintConfig.apiKey,
       },
-    }
-  );
+    });
 
-  return res;
+    return res;
+  }
+};
+
+export const acceptTOS = async (wallet: string) => {
+  if (crossmintConfig.apiKey) {
+    const res = await client.post(
+      `/tos/accept`,
+      {
+        locator: `sol:${wallet}`,
+        marketplaceName: 'Holaplex',
+      },
+      {
+        headers: {
+          'x-api-key': crossmintConfig.apiKey,
+        },
+      }
+    );
+
+    return res;
+
+  }
 };
