c54d8d5203ef73785cf9046fd53f7f739a0bad65
Anshul Goel <anshul.go87@gmail.com>
2022-08-08 12:46:34 +0530

    refactor: follow profiles in batches of 5

--- src/components/FollowAllButton.tsx
+++ src/components/FollowAllButton.tsx
@@ -7,6 +7,8 @@ import * as anchor from '@project-serum/anchor';
 import { Action, MultiTransactionContext } from 'src/views/_global/MultiTransaction';
 import { shortenAddress } from '@/modules/utils/string';
 import { useConnectedWalletProfile } from '../views/_global/ConnectedWalletProfileProvider';
+import { Transaction } from '@solana/web3.js';
+import { useRouter } from 'next/router';
 
 export interface ProfileToFollow {
   address: string;
@@ -17,6 +19,7 @@ export interface ProfileToFollow {
 const FollowAllButton = (props: { profilesToFollow?: ProfileToFollow[] }) => {
   const { connection } = useConnection();
   const anchorWallet = useAnchorWallet();
+  const router = useRouter();
   const myPubkey = anchorWallet?.publicKey.toBase58() || '';
 
   const { connectedProfile } = useConnectedWalletProfile();
@@ -43,20 +46,42 @@ const FollowAllButton = (props: { profilesToFollow?: ProfileToFollow[] }) => {
       new anchor.AnchorProvider(connection, anchorWallet, { commitment: 'processed' })
     );
 
-    const followTx = async (targetPubKey: string) => {
-      await graphProgram.methods
-        .makeConnection(new anchor.web3.PublicKey(targetPubKey))
-        .accounts({ from: anchorWallet.publicKey })
-        .rpc();
+    const followTx = async (profileAddresses: string[]) => {
+      const txn = new Transaction();
+      const asyncFollowTxns = profileAddresses.map(async (address) => {
+        return await graphProgram.methods
+          .makeConnection(new anchor.web3.PublicKey(address))
+          .accounts({ from: anchorWallet.publicKey })
+          .transaction();
+      });
+
+      const followTxns = await Promise.all(asyncFollowTxns);
+      txn.add(...followTxns);
+
+      txn.feePayer = anchorWallet.publicKey;
+      const recentBlockhash = await connection.getLatestBlockhash();
+      txn.recentBlockhash = recentBlockhash.blockhash;
+      const signedTransaction = await anchorWallet.signTransaction(txn);
+
+      const txtId = await connection.sendRawTransaction(signedTransaction.serialize());
+      if (txtId) await connection.confirmTransaction(txtId, 'confirmed');
     };
-    let actions: Action[] = actualProfilesToFollow.map((profile) => {
-      return {
-        name: `Following ${profile.handle || shortenAddress(profile.address)}`,
-        id: `follow${profile.address}}`,
+
+    let actions: Action[] = [];
+    const profilesArray = [...actualProfilesToFollow];
+    while (profilesArray.length) {
+      const batch = profilesArray.splice(0, 5);
+      const profileAddresses = batch.map((p) => p.address);
+      const namesArray = batch.map((p) => p.handle || shortenAddress(p.address));
+      const names = namesArray.join(', ');
+
+      actions.push({
+        name: `Following ${names}`,
+        id: `follow${batch[0].address}}`,
         action: followTx,
-        param: profile.address,
-      };
-    });
+        param: profileAddresses,
+      });
+    }
 
     await runActions(actions, {
       onActionSuccess: async () => {
@@ -67,6 +92,7 @@ const FollowAllButton = (props: { profilesToFollow?: ProfileToFollow[] }) => {
       },
       onComplete: async () => {
         //await props.refetch();
+        router.reload();
       },
     });
   };
